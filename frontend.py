62d8ec74f7c01fce6e62c69016f02f5b5550bc10
Icaro Davies <idavies@outook.com>
2026-04-13 22:09:17 -0300

    wip: registrar voto

--- frontend.py
+++ frontend.py
@@ -24,7 +24,7 @@ async def main (page: ft.Page):
             for tecnologia, votos in dados.items():
                 linha = ft.Row([
                     ft.Text(f"{tecnologia} : {votos}", size=18, expand=True),
-                    ft.Button(content="Votar", data=tecnologia)
+                    ft.Button(content="Votar", data=tecnologia, on_click=registrar_voto)
                 ])
                 coluna_placar.controls.append(linha)
 
@@ -34,6 +34,22 @@ async def main (page: ft.Page):
             coluna_placar.controls.append(ft.Text("Erro ao conectar no banco/backend", color="red"))
             page.update()
 
+    async def registrar_voto(e):
+        """Registrar Voto"""
+        tecnologia = e.control.data
+
+        try:
+            async with httpx.AsyncClient as client:
+                resposta = await client.post(f"{API_URL}/votar", json={"tecnologia": tecnologia})
+            dados = resposta.json()
+
+            if dados.get("sucesso"):
+                await carregar_placar()
+            else:
+                page.show_dialog(ft.SnackBar(content=ft.Text(dados.get("mensagem", "Erro desconhecido"))))
+        except Exception:
+            page.show_dialog(ft.SnackBar(content=ft.Text(f"Falha de conexão ao votar em {tecnologia}")))
+    
     page.add(texto_titulo, ft.Divider(), coluna_placar)
 
     await carregar_placar()
